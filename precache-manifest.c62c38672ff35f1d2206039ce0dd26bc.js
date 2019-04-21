ad12c5d18cccecf72f42530afd5c5241f65d7e7c
João Paulo <joao.schmidt@viaflow.com.br>
2019-04-21 17:53:10 -0300

    Updates

---
+++ precache-manifest.c62c38672ff35f1d2206039ce0dd26bc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f7220a0dd725a5b0176",
+    "url": "/ekki_web_client/static/js/runtime~main.3c0f1d3f.js"
+  },
+  {
+    "revision": "04353e10be86f4b3ee1e",
+    "url": "/ekki_web_client/static/js/main.f71b0f00.chunk.js"
+  },
+  {
+    "revision": "dc52af9b1f121b944d21",
+    "url": "/ekki_web_client/static/js/2.f85f44bb.chunk.js"
+  },
+  {
+    "revision": "5e934bdcf3216fc162b0df6f63504511",
+    "url": "/ekki_web_client/index.html"
+  }
+];
