76412af8d3fb5eb42988a9497e815c36589283f4
João Paulo <joao.schmidt@viaflow.com.br>
2019-04-21 17:49:28 -0300

    Updates

---
+++ precache-manifest.d127b38086c5240bcd4427b43a6fb9ce.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "04353e10be86f4b3ee1e",
+    "url": "/static/js/main.f71b0f00.chunk.js"
+  },
+  {
+    "revision": "dc52af9b1f121b944d21",
+    "url": "/static/js/2.f85f44bb.chunk.js"
+  },
+  {
+    "revision": "1fb6ffaf5a09563e60a1f77636d92f76",
+    "url": "/index.html"
+  }
+];
